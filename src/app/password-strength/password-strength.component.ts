154fc5ceea14edb8c7a45f69b73634f0c162150d
Maks <Maks@DESKTOP-597G7RS>
2023-07-28 17:59:52 +0300

    added ukrainian

--- src/app/password-strength/password-strength.component.ts
+++ src/app/password-strength/password-strength.component.ts
@@ -1,25 +1,30 @@
-import { Component, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { PasswordState } from 'src/shared/constants';
 
 @Component({
   selector: 'password-strength-component',
   templateUrl: './password-strength.component.html',
-  styleUrls: ['./password-strength.component.css']
+  styleUrls: ['./password-strength.component.css'],
 })
-
 export class PasswordStrengthComponent implements OnChanges {
-  @Input() password: string = "";
+  @Input() password: string = '';
 
-  section1: string = "";
-  section2: string = "";
-  section3: string = "";
+  section1Color: string = '';
+  section2Color: string = '';
+  section3Color: string = '';
 
   missingParts: string[] = [];
 
-  @Output() explanation: string = "";
+  @Output() explanation: string = '';
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.password = changes["password"].currentValue;
+    this.password = changes['password'].currentValue;
     let force = this.CheckPasswordStrength(this.password);
     this.SetSectionsColors(force);
   }
@@ -41,46 +46,38 @@ export class PasswordStrengthComponent implements OnChanges {
     const numbers = /[0-9]+/.test(this.password);
     const symbols = /\W|_/g.test(this.password);
 
-    letters ? force += 1 : this.missingParts.push($localize`letters`);
-    numbers ? force += 1 : this.missingParts.push($localize`numbers`);
-    symbols ? force += 1 : this.missingParts.push($localize`symbols`);
+    letters ? (force += 1) : this.missingParts.push($localize`letters`);
+    numbers ? (force += 1) : this.missingParts.push($localize`numbers`);
+    symbols ? (force += 1) : this.missingParts.push($localize`symbols`);
     return force;
   }
 
   SetSectionsColors(force: Number) {
-    this.explanation = "";
+    this.explanation = '';
     if (force === PasswordState.Empty) {
-      this.section1 = "revert";
-      this.section2 = "revert";
-      this.section3 = "revert";
-    }
-
-    else if (force === PasswordState.Strong) {
-      this.section1 = "green";
-      this.section2 = "green";
-      this.section3 = "green";
-    }
-
-    else {
+      this.section1Color = 'revert';
+      this.section2Color = 'revert';
+      this.section3Color = 'revert';
+    } else if (force === PasswordState.Strong) {
+      this.section1Color = 'green';
+      this.section2Color = 'green';
+      this.section3Color = 'green';
+    } else {
       this.explanation = $localize`Password must contain: ${this.missingParts}`;
 
       if (force === PasswordState.Easy) {
-        this.section1 = "red";
-        this.section2 = "red";
-        this.section3 = "red";
-        this.explanation += $localize`(8 or more characters)`;
-      }
-
-      else if (force === PasswordState.MediumSingle) {
-        this.section1 = "red";
-        this.section2 = "revert";
-        this.section3 = "revert";
-      }
-
-      else if (force === PasswordState.MediumDouble) {
-        this.section1 = "yellow";
-        this.section2 = "yellow";
-        this.section3 = "revert";
+        this.section1Color = 'red';
+        this.section2Color = 'red';
+        this.section3Color = 'red';
+        this.explanation += $localize`(8 or more)`;
+      } else if (force === PasswordState.MediumSingle) {
+        this.section1Color = 'red';
+        this.section2Color = 'revert';
+        this.section3Color = 'revert';
+      } else if (force === PasswordState.MediumDouble) {
+        this.section1Color = 'yellow';
+        this.section2Color = 'yellow';
+        this.section3Color = 'revert';
       }
     }
   }
