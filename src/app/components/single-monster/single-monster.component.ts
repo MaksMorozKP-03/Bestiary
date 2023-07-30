fec8e5a700c1ab70ff5d7934cd599f293b61b434
Maks <Maks@DESKTOP-597G7RS>
2023-07-30 13:42:00 +0300

    update app structure

--- src/app/components/single-monster/single-monster.component.ts
+++ src/app/components/single-monster/single-monster.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-single-monster',
   templateUrl: './single-monster.component.html',
-  styleUrls: ['./single-monster.component.css']
+  styleUrls: ['./single-monster.component.css'],
 })
 export class SingleMonsterComponent {
-
+  imagePath: string = '../../../assets/images/leshen.jpg';
 }
